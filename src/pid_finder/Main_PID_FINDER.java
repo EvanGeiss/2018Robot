40c0c4219db53f8c5fcd5e16e43ef28942dd40d6
EGeiss <egeiss03@gmail.com>
2018-03-10 21:58:34 -0500

    Yeet

---
+++ src/pid_finder/Main_PID_FINDER.java
@@ -0,0 +1,11 @@
+package pid_finder;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team5263.robot.subsystems.*;
+import org.usfirst.frc.team5263.robot.commands.*;
+
+public class Main_PID_FINDER extends CommandGroup{
+	public Main_PID_FINDER() {
+		
+	}
+}
---
+++ src/pid_finder/pid_finder.java
@@ -0,0 +1,33 @@
+package pid_finder;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Created by Evan Geiss
+ * Worked upon by Team 5263
+ * Property of Team 5263 - Riverview Robobucs
+ */
+public class pid_finder extends Command {
+
+    public pid_finder() {
+        
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    
+    protected void end() {
+    }
+
+    
+    protected void interrupted() {
+    }
+}
